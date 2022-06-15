b30973963d4e65193a4b91bb220afafb0f44fb90
Wojciech Brzuszkiewicz <wojciech.brzuszkiewicz@hotmail.com>
2022-06-15 16:34:41 +0200

    Adding array of commands to unit res

--- Resources/UnitRes/UnitRes.gd
+++ Resources/UnitRes/UnitRes.gd
@@ -6,3 +6,5 @@ export var max_movement_speed: float = 0.0;
 export var unit_icon: Texture = null;
 export var unit_look: Texture = null;
 export var unit_name: String = "Unit";
+
+export(Array, Resource) var unit_commads;
